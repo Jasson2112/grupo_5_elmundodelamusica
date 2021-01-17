68a59d4c621f53c68d24210f79db8dbd3fafd10a
Diego-Mattiacci <dmattiacci@hotmail.com>
2021-01-17 01:17:46 -0300

    Maquetado HTML 17-01

--- app.js
+++ app.js
@@ -18,3 +18,11 @@ app.get('/login', (req,res)=>{
 app.get('/register', (req,res)=>{
     res.sendFile(__dirname + '/views/register.html');
 });
+
+app.get('/productCart', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/productDetail', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
