959fb59ead2ca41133e5742df1d588719068801f
Diego-Mattiacci <dmattiacci@hotmail.com>
2021-01-27 21:21:40 -0300

    productCreate

--- Site/src/controllers/mainController.js
+++ Site/src/controllers/mainController.js
@@ -5,23 +5,5 @@ module.exports={
     home: (req,res)=>{
         res.render("home");
     },
-    /*login: (req,res)=>{
-        res.render("login");
-    },
-    register: (req,res)=>{
-        res.render("register");
-    },
-    productCart: (req,res)=>{
-        res.render("productCart");
-    },
-    productDetail: (req,res)=>{
-        res.render("productDetail");
-    },
-    products: (req,res)=>{
-        res.render("products");
-    },
-    tutorial: (req,res)=>{
-        res.render("tutorial");
-    }*/
     
 }
--- Site/src/routes/mainRouter.js
+++ Site/src/routes/mainRouter.js
@@ -4,17 +4,7 @@ const maincontroller= require("../controllers/mainController");
 
 router.get("/", maincontroller.home);
 
-/*router.get("/login", maincontroller.login);
 
-router.get("/register", maincontroller.register);
-
-router.get("/productCart", maincontroller.productCart);
-
-router.get("/productDetail", maincontroller.productDetail);
-
-router.get("/products", maincontroller.products);
-
-router.get("/tutorial", maincontroller.tutorial);*/
 
 
 
