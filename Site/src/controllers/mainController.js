94dd84c6ef483fb33915b64364f648045383528c
Diego-Mattiacci <dmattiacci@hotmail.com>
2021-02-06 21:30:16 -0300

    Home con Archivos JSON products

--- Site/src/controllers/mainController.js
+++ Site/src/controllers/mainController.js
@@ -1,9 +1,17 @@
 
+const fs = require("fs")
+const path= require("path")
+
+let products= fs.readFileSync(path.join( __dirname, "../database/products.json") ,"utf-8");
+products= JSON.parse(products)
+
 
 module.exports={
     
     home: (req,res)=>{
-        res.render("home");
-    },
+        res.render("home", {
+            title:"Listado de Productos",
+            products});
+    }
     
 }
--- Site/src/controllers/productsController.js
+++ Site/src/controllers/productsController.js
@@ -1,3 +1,5 @@
+
+
 module.exports={
     
     productCart: (req,res)=>{
